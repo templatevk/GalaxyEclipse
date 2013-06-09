ca3bce4120a01729b5d888481f261e9c0fe9e367
sp209-16 <spaun1991t@gmail.com>
2013-06-09 17:46:50 +0300

    Implemented rendering every 25 ms.

--- Client/src/main/java/arch/galaxyeclipse/client/sounds/TestSound.java
+++ Client/src/main/java/arch/galaxyeclipse/client/sounds/TestSound.java
@@ -1,16 +1,25 @@
 package arch.galaxyeclipse.client.sounds;
 
 import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.Files;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.Input;
+import com.badlogic.gdx.audio.Music;
 import com.badlogic.gdx.audio.Sound;
 
 public class TestSound implements ApplicationListener {
 
     private Sound shoot;
+    private Music music;
+
+
     @Override
     public void create() {
         shoot = Gdx.audio.newSound(Gdx.files.internal("assets/sounds/shoot.mp3"));
+        music = Gdx.audio.newMusic(Gdx.files.getFileHandle("assets/sounds/shoot.mp3", Files.FileType.Internal));
+        music.setVolume(0.5f);
+        music.play();
+        music.setLooping(true);
     }
 
     @Override
